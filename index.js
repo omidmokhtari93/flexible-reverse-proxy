e86a3d1e2baf5af1543bf4c3b3d493b3d1dcc94d
Omid Mokhtari <omidmokhtari93@gmail.com>
2025-08-11 11:50:14 +0330

    Update version to 1.2.0, refactor routing logic to use path-to-regexp for improved route matching, and enhance API route configuration. Added new routes for various API endpoints and ensured compatibility with the updated routing mechanism.

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const { createProxyServer } = require("./lib/proxy-server");
 
 // Example routes configuration
 const routes = {
-  "/api/v1/baseinfos": "http://10.234.233.109:8026",
-  "/api/v1/rules": "http://10.234.233.109:8026",
-  "/api/v1/discounts": "http://10.234.233.109:8026",
-  "/api/v1/giftcards": "http://10.234.233.109:8026",
-  "/account/api/v1/login/": "https://apimarket.mtnirancell.ir",
   "/api/v1/user/": "https://apimarket.mtnirancell.ir",
+  "/account/api/v1/logout/": "https://apimarket.mtnirancell.ir",
   "/account/api/v1/sso": "https://apimarket.mtnirancell.ir",
+  "/account/api/v1/login/": "https://apimarket.mtnirancell.ir",
+
+  "/api/v1/baseinfos": "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/discounts/:id": "http://10.234.233.109:8026",
+  "/api/v1/rules/:id": "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/giftcards/:id": "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/rules": "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/giftcards": "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/discounts": "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/categories": "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/rules/export": "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/discounts/export": "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/activations/add/public":
+    "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/activations/upload/private":
+    "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/discounts/add/public/percent":
+    "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/discounts/add/public/fixed":
+    "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/discounts/upload/private/fixed":
+    "https://apimarket.mtnirancell.ir/promotion-qat",
+  "/api/v1/discounts/upload/private/percent":
+    "https://apimarket.mtnirancell.ir/promotion-qat",
 };
 
 // Create and start the proxy server with example configuration
